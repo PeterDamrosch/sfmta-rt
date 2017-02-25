77682a76a7faf90d2f8a96b14a28858248a63557
PeterDamrosch <peter.damrosch@gmail.com>
2017-02-25 14:37:24 -0500

    repeat every 30 seconds working

--- sfmta-rt.py
+++ sfmta-rt.py
@@ -1,3 +1,4 @@
+import time
 import datetime
 import requests
 import sys
@@ -8,134 +9,73 @@ from sqlalchemy.orm import sessionmaker
 
 from model import *
 
-# Get a list of routes from the Nextbus API
+# Get a list of routes from the SF-Muni Nextbus API
 # Adapted from Matt Conway's gtfsrdb
 
-# Parse the database option
+# Parse options - database URL and wait time (default 30)
 p = OptionParser()
 p.add_option('-d', '--database', default=None, dest='dsn',
              help='Database connection string', metavar='DSN')
 
+p.add_option('-w', '--wait', default=30, type='int', metavar='SECS',
+             dest='timeout', help='Time to wait between requests (in seconds)')
 opts, args = p.parse_args()
 
+# Warning if no database specified
 if opts.dsn == None:
     print('No database specified!')
     sys.exit(1)
 
-# Connect to the database, create session, create table
+# Database connection
 engine = create_engine(opts.dsn)
 session = sessionmaker(bind=engine)()
 Base.metadata.create_all(engine)
 
-def get_route_list(agency_code):
-	# Build URL
-	base_url = "http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a="
-	routeList_url = base_url + agency_code
+# Send requests
+keep_running = True
+while keep_running:
 
-	# Send request
-	response = requests.get(routeList_url)
+	try:
+		# Send request
+		request_time = datetime.datetime.now()
+		vehiclePositions_url = "http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=sf-muni"
+		response = requests.get(vehiclePositions_url)
 
-	# Create XML tree
-	tree = xml.etree.ElementTree.fromstring(response.content)
+		# Create XML tree
+		vp_tree = xml.etree.ElementTree.fromstring(response.content)
+		vehicle_tree = vp_tree.findall('vehicle')
 
-	# Add tags (aka ids) and titles (full route names) to a dictionary
-	route_dict = {}
-	for route in tree.findall('route'):
-		tag = route.get('tag')
-		title = route.get('title')
-		route_dict[tag] = title
+		# Loop through tree pulling out each vehicle
+		for vehicle in vehicle_tree:
+			# Empty lead vehicle since they don't all have them (for two car trains)
+			#leading_vehicle = ''
+			#if vehicle.get('leadingVehicleId'):
+				#leading_vehicle = vehicle.get('leadingVehicleId')
 
-	# Return results
-	return(route_dict)
+			# Create row for vehicle
+			vehicle_update = VehiclePosition(
+				veh_id = vehicle.get('id'),
+				route_tag = vehicle.get('routeTag'),
+				dir_tag = vehicle.get('dirTag'),
+				pos_lat = vehicle.get('lat'),
+				pos_lon = vehicle.get('lon'),
+				pos_speed = vehicle.get('speedKmHr'),
+				last_report = vehicle.get('secsSinceReport'),
+				lead_veh = vehicle.get('leadingVehicleId'),
+				timestamp = request_time)
 
-request_time = datetime.datetime.now()
-def get_vehicle_positions(agency_code):
-	request_time = datetime.datetime.now()
-	base_url = "http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a="
-	vehiclePositions_url = base_url + agency_code
+			# Add row to session
+			session.add(vehicle_update)
 
-	# Send request
-	response = requests.get(vehiclePositions_url)
+		# Commit rows to database
+		session.commit()
 
-	# Create XML tree
-	tree = xml.etree.ElementTree.fromstring(response.content)
-	return(tree)
+		print('Added {} vehicle positions at {}'.format(str(len(vehicle_tree)), request_time))
 
-def parse_vehicle_tree_dict(vp_tree):
-	### Just a test for parsing, which works ###
-	### When I implement it for real, will want the for-loop
-		# and then create a new sqlalchemy instance of class VehiclePosition
-		# and add to database at the end of each loop
-	position_dict = {}
-	for vehicle in vp_tree.findall('vehicle'):
-		vehicle_id = vehicle.get('id')
-		vehicle_route = vehicle.get('routeTag')
-		direction_tag = vehicle.get('dirTag')
-		vp_lat = vehicle.get('lat')
-		vp_lon = vehicle.get('lon')
-		last_report = vehicle.get('secsSinceReport')
-		position_dict[vehicle_id] = {'vid': vehicle_id, 
-			'route': vehicle_route,
-			'direction': direction_tag,
-			'lat': vp_lat,
-			'lon': vp_lon,
-			'lastReport': last_report}
-	return(position_dict)
+	except:
+		print('Exception occurred in iteration')
 
-def add_to_database(vp_tree):
-	for vehicle in vp_tree.findall('vehicle'):
-		# Empty lead vehicle since they don't all have them (for two car trains)
-		leading_vehicle = ''
-		if vehicle.get('leadingVehicleId'):
-			leading_vehicle = vehicle.get('leadingVehicleId')
+	time.sleep(opts.timeout)
 
-		# Create row for vehicle
-		vehicle_update = VehiclePosition(
-			veh_id = vehicle.get('id'),
-			route_tag = vehicle.get('routeTag'),
-			dir_tag = vehicle.get('dirTag'),
-			pos_lat = vehicle.get('lat'),
-			pos_lon = vehicle.get('lon'),
-			pos_speed = vehicle.get('speedKmHr'),
-			last_report = vehicle.get('secsSinceReport'),
-			lead_veh = leading_vehicle,
-			timestamp = request_time)
-
-		# Add row to session
-		session.add(vehicle_update)
-
-	# Commit rows to database
-	session.commit()
 
 		
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Gets all the stops and inbound and outbound stop orders for that route
-# Can do for one route or up to 100 routes (SFMTA has 83 total)
-# Has things like the 
-# As well as paths - don't need this for now
-#def get_route_config(agency_code):
-	#base_url = "http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a="
-	#routeConfig = base_url + agency_code
-
-#http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=sf-muni
